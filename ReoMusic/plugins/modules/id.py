e1fba97e5c70921e6392e4d12682ddd4237adf22
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-19 00:12:44 +0530

    Update id.py

--- ReoMusic/plugins/modules/id.py
+++ ReoMusic/plugins/modules/id.py
@@ -1,7 +1,16 @@
-
-
 from ReoMusic import app
 from pyrogram import filters
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    InputMediaVideo,
+    Message,
+)
+import config
+from config import BANNED_USERS, CLEANMODE_DELETE_MINS, MUSIC_BOT_NAME, OWNER_ID
 
 
 @app.on_message(filters.command("id"))
@@ -15,3 +24,31 @@ def ids(_, message):
         message.reply(
             f"**ʏᴏᴜʀ ɪᴅ**: `{message.from_user.id}`\n**ᴄʜᴀᴛ ɪᴅ**: `{message.chat.id}`"
         )
+
+
+async def send_source_message(callback_query):
+    await callback_query.edit_message_media(
+        InputMediaVideo("https://graph.org/file/14070f920e9294e203166.mp4"),
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
+        ),
+    )
+
+@app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
+@languageCB
+async def gib_repo(client, callback_query, _):
+    await send_source_message(callback_query)
+
+@app.on_message(
+    filters.command("repo") & filters.group & ~filters.edited & ~BANNED_USERS
+)
+@language
+async def repo_cmds(client, message: Message, _):
+    await send_source_message(message)
+
+@app.on_message(
+    filters.command("source") & filters.group & ~filters.edited & ~BANNED_USERS
+)
+@language
+async def source_cmds(client, message: Message, _):
+    await send_source_message(message)
