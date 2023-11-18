c9159f562d0f65abb8385f8b6d12499ee462bcdf
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-18 23:10:18 +0530

    Add files via upload

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf ReoBots && git clone https://github.com/AbhiMod/ReoBots && cd ReoBots && python3 -m ReoMusic")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
