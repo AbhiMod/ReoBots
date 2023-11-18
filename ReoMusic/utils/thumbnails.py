f3b954aef851704e0673944efb97d5925a80dfa0
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-18 23:24:24 +0530

    Update thumbnails.py

--- ReoMusic/utils/thumbnails.py
+++ ReoMusic/utils/thumbnails.py
@@ -1,17 +1,18 @@
-
-
-
 import os
 import re
 import textwrap
-
+import random
 import aiofiles
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
+                 ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
+
+themes = ["bgreen", "blue", "colorfull", "dgreen", "hgreen", "lgreen", "lyellow", "orange", "pink", "purple", "rainbow", "red", "sky", "thumbnail", "yellow"]
+colors = ["white", "black", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
@@ -53,95 +54,36 @@ async def gen_thumb(videoid):
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
                     await f.write(await resp.read())
                     await f.close()
 
-        youtube = Image.open(f"cache/thumb{videoid}.png")
-        image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(50))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.9)
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.ANTIALIAS)
-        logo = ImageOps.expand(logo, border=17, fill="pink")
-        background.paste(logo, (50, 100))
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 70)
-        arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("assets/font.ttf", 40)
-        para = textwrap.wrap(title, width=32)
-        j = 0
-        draw.text(
-            (6, 6), f"Powered By:- © ReoBots", fill="Yellow", font=name_font
-        )
-        draw.text(
-            (600, 150),
-            f"Reo Music",
-            fill="white",
-            stroke_width=2,
-            stroke_fill="yellow",
-            font=font2,
-        )
-        for line in para:
-            if j == 1:
-                j += 1
-                draw.text(
-                    (600, 340),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if j == 0:
-                j += 1
-                draw.text(
-                    (600, 280),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-
-        draw.text(
-            (600, 450),
-            f"Views : {views[:23]}",
-            fill="white",
-            stroke_width=1,
-            stroke_fill="white",
-            font=font,
-        )
-        draw.text(
-            (600, 500),
-            f"Duration : {duration[:23]} Mins",
-            fill="white",
-            stroke_width=1,
-            stroke_fill="white",
-            font=font,
-        )
-        draw.text(
-            (600, 550),
-            f"Owner : Dr Asad Ali",
-            fill="white",
-            stroke_width=1,
-            stroke_fill="white",
-            font=font,
-        )
+        images = random.choice(themes)
+        border = random.choice(colors)
+        image1 = Image.open(f"cache/thumb{videoid}.png")
+        image2 = Image.open(f"assets/reo/{images}.png")
+        image3 = changeImageSize(1280, 720, image1)
+        image4 = changeImageSize(1280, 720, image2)
+        image5 = image3.convert("RGBA")
+        image6 = image4.convert("RGBA")
+        Image.alpha_composite(image5, image6).save("cache/temp.png")
+        logo = Image.open("cache/temp.png")
+        img = ImageOps.expand(logo, border=10, fill=f"{border}")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("assets/reo/font.otf", 55)
+        font2 = ImageFont.truetype("assets/reo/font.otf", 30)
+        draw.text((20, 555), f"Title: {title[:50]} ...", (255, 255, 255), font=font)
+        draw.text((20, 615), f"Duration: {duration}", (255, 255, 255), font=font)
+        draw.text((20, 675), f"Views: {views}", (255, 255, 255), font=font)
+        draw.text((10, 10), f"Reo•MUSIC", (255, 255, 255), font=font2)
         try:
             os.remove(f"cache/thumb{videoid}.png")
+            os.remove(f"cache/temp.png")
         except:
             pass
-        background.save(f"cache/{videoid}.png")
+        img.save(f"cache/{videoid}.png")
         return f"cache/{videoid}.png"
     except Exception:
         return YOUTUBE_IMG_URL
