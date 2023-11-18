9291bcb1de2e2b24532ca1dbd0cfc7240061a2d5
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-19 00:18:43 +0530

    Update id.py

--- ReoMusic/plugins/modules/id.py
+++ ReoMusic/plugins/modules/id.py
@@ -25,24 +25,27 @@ def ids(_, message):
 
 async def send_source_message(callback_query):
     await callback_query.edit_message_media(
-        InputMediaVideo("https://graph.org/file/14070f920e9294e203166.mp4"),
+        media=InputMediaVideo("https://graph.org/file/14070f920e9294e203166.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
     )
 
 @app.on_callback_query(filters.regex("gib_source") & ~BANNED_USERS)
-async def gib_repo(_, callback_query):
+async def gib_repo(_, callback_query: CallbackQuery):
     await send_source_message(callback_query)
 
 @app.on_message(
     filters.command("repo") & filters.group & ~filters.edited & ~BANNED_USERS
 )
-async def repo_cmds(_, message):
+async def repo_cmds(_, message: Message):
+    # Since message does not have edit_message_media method, you can't use it here
+    # Instead, you can send a new message or use other appropriate methods
     await send_source_message(message)
 
 @app.on_message(
     filters.command("source") & filters.group & ~filters.edited & ~BANNED_USERS
 )
-async def source_cmds(_, message):
+async def source_cmds(_, message: Message):
+    # Same as above, if you want to edit the original message, you can't use edit_message_media
     await send_source_message(message)
