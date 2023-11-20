a8bd429e83367d426d06a875e6f6daaf1b8c46c4
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-21 00:07:33 +0530

    Update start.py

--- ReoMusic/plugins/bot/start.py
+++ ReoMusic/plugins/bot/start.py
@@ -191,20 +191,27 @@ Pᴏᴡᴇʀᴇᴅ ʙʏ {config.MUSIC_BOT_NAME}"""
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
-                        message.from_user.mention, f"[{BOT_NAME}](https://t.me/{bot_mention})", f"[{BOT_NETWORK}](https://t.me/ReoBots)"
+                        f"[{BOT_NETWORK}](https://t.me/ReoBots)", 
+                        message.from_user.mention, 
+                        f"[{BOT_NAME}](https://t.me/{bot_mention})"
                     ),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
             except:
                 await message.reply_text(
                     _["start_2"].format(
-                        message.from_user.mention,f"[{BOT_NAME}](https://t.me/{bot_mention})", f"[{BOT_NETWORK}](https://t.me/ReoBots)"
+                       f"[{BOT_NETWORK}](https://t.me/ReoBots)",  
+                        message.from_user.mention,
+                        f"[{BOT_NAME}](https://t.me/{bot_mention})"
                     ),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
         else:
             await message.reply_text(
-                _["start_2"].format(message.from_user.mention, f"[{BOT_NAME}](https://t.me/{bot_mention})", f"[{BOT_NETWORK}](https://t.me/ReoBots)"),
+                _["start_2"].format(f"[{BOT_NETWORK}](https://t.me/ReoBots)",
+                                    message.from_user.mention, 
+                                    f"[{BOT_NAME}](https://t.me/{bot_mention})"
+                                   ),
                 reply_markup=InlineKeyboardMarkup(out),
             )
         if await is_on_off(config.LOG):
