f6c1c25a3599d50f7ee7239650bfe615af74b9fa
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-08 14:46:52 +0530

    Update start.py

--- ReoMusic/plugins/bot/start.py
+++ ReoMusic/plugins/bot/start.py
@@ -84,9 +84,10 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
+        STATS = f"[Here](https://t.me/ReoStatusBots/2)"
         await message.reply_photo(
             photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
+            caption=_["start_2"].format(message.from_user.mention, app.mention,STATS),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
