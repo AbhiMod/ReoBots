ab70d00eaadd33ee2314526864b699d1cb58d81c
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-08 15:46:57 +0530

    Update start.py

--- ReoMusic/utils/inline/start.py
+++ ReoMusic/utils/inline/start.py
@@ -26,6 +26,7 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_10"], url=config.REOSTATS),
         ],
     ]
     return buttons
